82742c969170b01d6c8c5e4c235d577d3957fcfb
aadamcz <33779500+aadamcz@users.noreply.github.com>
2017-12-02 11:08:40 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var buttonContent = document.getElementsByClassName('button');
 
 var i = 0;
-for (i=0 ; i < buttons.length; i++ ){
+for (i = 0 ; i < buttons.length; i++ ){
 	alert(buttons[i].innerText);
 };
